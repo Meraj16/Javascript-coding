3685e192edb4033feb6d788d7383348b4cc44930
Meraj16 <72215899+Meraj16@users.noreply.github.com>
2021-10-27 18:28:47 +0530

    Update removeDuplicatesFromArray.js

--- removeDuplicatesFromArray.js
+++ removeDuplicatesFromArray.js
@@ -16,3 +16,25 @@ console.log(uniq)
 
 
 // Output  = [1,3,2]
+
+
+
+//FULL explanation 
+
+// var arr = [5,6,5,3,8,2,8,6]
+
+// var uniq = arr.filter(function(item, pos) {
+    
+//     console.log('before-------> ','Number-',item, 'index',pos)
+//     if(arr.indexOf(item) == pos){
+    
+//     console.log('yes','Number-',item,'==', 'index',pos)
+        
+//         return item
+//     } else {
+//         console.log('no ','Number-',item, 'index',arr.indexOf(item))
+//     }
+    
+// })
+
+// console.log(uniq)
